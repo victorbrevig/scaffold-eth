0312741e612a39bec3f6f3a9541d16fafe3c2398
victorbrevig <victorbrevig@gmail.com>
2021-12-28 23:14:29 +0100

    svg code and generator added

--- packages/hardhat/contracts/YourCollectible.sol
+++ packages/hardhat/contracts/YourCollectible.sol
@@ -7,8 +7,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import 'base64-sol/base64.sol';
 
+
 import './HexStrings.sol';
 import './ToColor.sol';
+import './SVGgenerator.sol';
+
 //learn more: https://docs.openzeppelin.com/contracts/3.x/erc721
 
 // GET LISTED ON OPENSEA: https://testnets.opensea.io/get-listed/step-two
@@ -21,63 +24,73 @@ contract YourCollectible is ERC721Enumerable, Ownable {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  // all funds go to buidlguidl.eth
+  SVGgenerator svgGenerator;
+
+  
   address payable public constant recipient =
-    payable(0xa81a6a910FeD20374361B35C451a4a44F86CeD46);
+    payable(0x93F82f49f40be5a5e6f1e665AcCf96f56b2ae721);
+
 
   uint256 public constant limit = 3728;
   uint256 public constant curve = 1002; // price increase 0,4% with each purchase
   uint256 public price = 0.001 ether;
-  // the 1154th optimistic loogies cost 0.01 ETH, the 2306th cost 0.1ETH, the 3459th cost 1 ETH and the last ones cost 1.7 ETH
 
-  mapping (uint256 => bytes3) public color;
+  constructor() ERC721("Bloopers", "BLOOP") {
+    
+  }
+
+
+
+
+  mapping (uint256 => bytes3) public bodyColor;
+  mapping (uint256 => bytes3) public gradientColor1;
+  mapping (uint256 => bytes3) public gradientColor2;
+
+  mapping (uint256 => uint8) public tiers;
+  mapping (uint256 => uint8) public hats;
+  mapping (uint256 => uint8) public faces;
+
   mapping (uint256 => uint256) public chubbiness;
   mapping (uint256 => uint256) public mouthLength;
 
-  constructor() public ERC721("OptimisticLoogies", "OPLOOG") {
-    // RELEASE THE OPTIMISTIC LOOGIES!
+  
+
+
+  function mintItem() public payable returns (uint256) {
+    require(_tokenIds.current() < limit, "DONE MINTING");
+    require(msg.value >= price, "NOT ENOUGH");
+    price = (price * curve) / 1000;
+
+    _tokenIds.increment();
+
+    uint256 id = _tokenIds.current();
+    _mint(msg.sender, id);
+
+    bytes32 predictableRandom = keccak256(abi.encodePacked( blockhash(block.number-1), msg.sender, address(this), id ));
+    bodyColor[id]      = bytes2(predictableRandom[0]) | ( bytes2(predictableRandom[1]) >> 8 ) | ( bytes3(predictableRandom[2]) >> 16 );
+    gradientColor1[id] = bytes2(predictableRandom[3]) | ( bytes2(predictableRandom[4]) >> 8 ) | ( bytes3(predictableRandom[5]) >> 16 );
+    gradientColor2[id] = bytes2(predictableRandom[6]) | ( bytes2(predictableRandom[7]) >> 8 ) | ( bytes3(predictableRandom[8]) >> 16 ); 
+    tiers[id]          = uint8(predictableRandom[9])%3;
+    hats[id]           = uint8(predictableRandom[10])%10;
+    faces[id]          = uint8(predictableRandom[11])%11;
+    chubbiness[id] = 35+((55*uint256(uint8(predictableRandom[3])))/255);
+    mouthLength[id] = 180+((uint256(chubbiness[id]/4)*uint256(uint8(predictableRandom[4])))/255);
+    // Send to recipient address
+    (bool success, ) = recipient.call{value: msg.value}("");
+    require(success, "could not send");
+    return id;
   }
 
-  function mintItem()
-      public
-      payable
-      returns (uint256)
-  {
-      require(_tokenIds.current() < limit, "DONE MINTING");
-      require(msg.value >= price, "NOT ENOUGH");
-
-      price = (price * curve) / 1000;
-
-      _tokenIds.increment();
-
-      uint256 id = _tokenIds.current();
-      _mint(msg.sender, id);
-
-      bytes32 predictableRandom = keccak256(abi.encodePacked( id, blockhash(block.number-1), msg.sender, address(this) ));
-      color[id] = bytes2(predictableRandom[0]) | ( bytes2(predictableRandom[1]) >> 8 ) | ( bytes3(predictableRandom[2]) >> 16 );
-      chubbiness[id] = 35+((55*uint256(uint8(predictableRandom[3])))/255);
-      // small chubiness loogies have small mouth
-      mouthLength[id] = 180+((uint256(chubbiness[id]/4)*uint256(uint8(predictableRandom[4])))/255);
-
-      (bool success, ) = recipient.call{value: msg.value}("");
-      require(success, "could not send");
-
-      return id;
-  }
-
+  
+  
   function tokenURI(uint256 id) public view override returns (string memory) {
       require(_exists(id), "not exist");
       string memory name = string(abi.encodePacked('Loogie #',id.toString()));
-      string memory description = string(abi.encodePacked('This Loogie is the color #',color[id].toColor(),' with a chubbiness of ',uint2str(chubbiness[id]),' and mouth length of ',uint2str(mouthLength[id]),'!!!'));
+      string memory description = string(abi.encodePacked('This Loogie is the color #',bodyColor[id].toColor(),' with a chubbiness of ',uint2str(chubbiness[id]),' and mouth length of ',uint2str(mouthLength[id]),'!!!'));
       string memory image = Base64.encode(bytes(generateSVGofTokenById(id)));
 
-      return
-          string(
+      string memory firstPart = string(
               abi.encodePacked(
-                'data:application/json;base64,',
-                Base64.encode(
-                    bytes(
-                          abi.encodePacked(
                               '{"name":"',
                               name,
                               '", "description":"',
@@ -85,61 +98,55 @@ contract YourCollectible is ERC721Enumerable, Ownable {
                               '", "external_url":"https://burnyboys.com/token/',
                               id.toString(),
                               '", "attributes": [{"trait_type": "color", "value": "#',
-                              color[id].toColor(),
+                              bodyColor[id].toColor(),
                               '"},{"trait_type": "chubbiness", "value": ',
                               uint2str(chubbiness[id]),
                               '},{"trait_type": "mouthLength", "value": ',
                               uint2str(mouthLength[id]),
-                              '}], "owner":"',
+                              '}], "owner":"'));
+      string memory secondPart = string(
+              abi.encodePacked(
                               (uint160(ownerOf(id))).toHexString(20),
                               '", "image": "',
                               'data:image/svg+xml;base64,',
                               image,
-                              '"}'
-                          )
-                        )
+                              '"}'));
+      
+      return
+          string(
+              abi.encodePacked(
+                'data:application/json;base64,',
+                Base64.encode(
+                    bytes(string(abi.encodePacked(firstPart,secondPart)))
                     )
               )
           );
+          
+          
   }
 
   function generateSVGofTokenById(uint256 id) internal view returns (string memory) {
+    
+    string memory svgP1 = string(abi.encodePacked(
+      '<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1080 1080"><title>Bloops</title><defs><linearGradient id="linear-gradient" x2="1080" y2="1080" gradientUnits="userSpaceOnUse">',
+      '<stop offset="0" stop-color="#',gradientColor1[id].toColor(),'" />',
+      '<stop offset="1" stop-color="#',gradientColor2[id].toColor(),'" />'
+      ));
 
-    string memory svg = string(abi.encodePacked(
-      '<svg width="400" height="400" xmlns="http://www.w3.org/2000/svg">',
-        renderTokenById(id),
-      '</svg>'
+    string memory svgP2 = string(abi.encodePacked(
+      '</linearGradient></defs><rect width="1080" height="1080" fill="url(#linear-gradient)" /><g stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="10"><g fill="#',
+      bodyColor[id].toColor(),'">',
+      svgGenerator.renderTokenBodyById(tiers[id]),
+      '</g>',
+      svgGenerator.renderTokenHatById(hats[id]),
+      svgGenerator.renderTokenFaceById(faces[id]),
+      '</g></svg>'
     ));
-
-    return svg;
+    
+    return string(abi.encodePacked(svgP1,svgP2));
+    
   }
 
-  // Visibility is `public` to enable it being called by other contracts for composition.
-  function renderTokenById(uint256 id) public view returns (string memory) {
-    // the translate function for the mouth is based on the curve y = 810/11 - 9x/11
-    string memory render = string(abi.encodePacked(
-      '<g id="eye1">',
-        '<ellipse stroke-width="3" ry="29.5" rx="29.5" id="svg_1" cy="154.5" cx="181.5" stroke="#000" fill="#fff"/>',
-        '<ellipse ry="3.5" rx="2.5" id="svg_3" cy="154.5" cx="173.5" stroke-width="3" stroke="#000" fill="#000000"/>',
-      '</g>',
-      '<g id="head">',
-        '<ellipse fill="#',
-        color[id].toColor(),
-        '" stroke-width="3" cx="204.5" cy="211.80065" id="svg_5" rx="',
-        chubbiness[id].toString(),
-        '" ry="51.80065" stroke="#000"/>',
-      '</g>',
-      '<g id="eye2">',
-        '<ellipse stroke-width="3" ry="29.5" rx="29.5" id="svg_2" cy="168.5" cx="209.5" stroke="#000" fill="#fff"/>',
-        '<ellipse ry="3.5" rx="3" id="svg_4" cy="169.5" cx="208" stroke-width="3" fill="#000000" stroke="#000"/>',
-      '</g>'
-      '<g class="mouth" transform="translate(',uint256((810-9*chubbiness[id])/11).toString(),',0)">',
-        '<path d="M 130 240 Q 165 250 ',mouthLength[id].toString(),' 235" stroke="black" stroke-width="3" fill="transparent"/>',
-      '</g>'
-    ));
-
-    return render;
-  }
 
   function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
       if (_i == 0) {
@@ -162,4 +169,5 @@ contract YourCollectible is ERC721Enumerable, Ownable {
       }
       return string(bstr);
   }
+  
 }
