25418a8427a860cbc38f197166609cb9f20b5a0e
jcer10 <juan.cervera.2@gmail.com>
2022-01-10 13:47:59 +0100

    ...

--- packages/hardhat/contracts/YourCollectible.sol
+++ packages/hardhat/contracts/YourCollectible.sol
@@ -172,7 +172,7 @@ contract YourCollectible is ERC721Enumerable, Ownable {
         "484872"
     ];
 
-    uint8 constant noOfHats = 19;
+    uint8 constant noOfHats = 20;
     uint8 constant noOfEyes = 16;
     uint8 constant noOfMouths = 17;
     uint8 constant noOfFullFaces = 7;
