5bb766a50eec479eb13b146b052d31c88a98c767
jcer10 <juan.cervera.2@gmail.com>
2022-02-07 12:53:20 +0100

    tru

--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -5,7 +5,7 @@ import { Button, Typography, Card, List, Col, Row } from "antd";
 import { Address } from "../components";
 import { ethers } from "ethers";
 import "../App.css";
-import GIF from "../blobbers.gif";
+import BOX from "../upgrade.svg";
 import BLOBBER from "../blobber.svg";
 import COIN from "../coin.svg";
 
@@ -31,7 +31,7 @@ function Home({ readContracts, mainnetProvider, blockExplorer, totalSupply, DEBU
                             <img className="imagesHome" src={BLOBBER} alt="BlobberAnim" />
                             <Title level={2} style={{ fontSize: 34, marginRight: 8 }}>What are Blobbers?</Title>
                             <Paragraph style={{ fontSize: 18 }}>
-                                Planet Blob-6 has become unhabitable due to global warming... sounds familiar? All of its inhabitants, the Blobbers, have had to migrate to other planets in the known universe where life is prosperous. Earth has caught their attention, and as they start their migration to our planet, the Blobbers are getting used to our weird clothes and consumer products. It seems that all they want is to find an owner that adopts them and in return, they'll produce some sort of BLOB which to them is just their poop, but to us humans it has value, it's cryptocurrency! Mint and collect these blobbers, each with their own traits, some more rare than others and help the blobbers carry on their legacy as the first life forms to excrete cryptocurrency.
+                                Planet Blob-6 has become unhabitable due to global warming... sounds familiar? All of its inhabitants, the Blobbers, have had to migrate to other planets in the known universe where life is prosperous. Earth has caught their attention, and as they start their migration to our planet, the Blobbers are getting used to our weird clothes and consumer products. It seems that all they want is to find an owner that adopts them and in return, they'll produce some sort of BLOB which to them is just their poop, but to us humans it has value, it's cryptocurrency! Mint and collect these blobbers, each with their own traits, some rarer than others and help the blobbers carry on their legacy as the first life forms to excrete cryptocurrency.
                             </Paragraph>
                             <Paragraph style={{ fontSize: 16 }}>
                                 Blobbers is a diverse NFT collection on the Optimism network. Each blobber is drawn from SVG code which is fully on-chain. This allows the owner of a blobber to change it's apperance through upgrades.
@@ -49,7 +49,11 @@ function Home({ readContracts, mainnetProvider, blockExplorer, totalSupply, DEBU
                     </Col>
                     <Col span={8}>
                         <Card>
-                            Card content
+                            <img className="imagesHome" src={BOX} alt="BoxAnim" />
+                            <Title level={2} style={{ fontSize: 34, marginRight: 8 }}>Upgrade to Tier 2</Title>
+                            <Paragraph style={{ fontSize: 18 }}>
+                                Every Blobber can be upgraded once, and the cost of this upgrade is XXX BLB. By upgrading your NFT, the SVG code that draws your Blobber will be modified to render a Tier 2 Blobber. These Tier 2 Blobbers are characterized by having a gradient background. On top of that, your Blobber will randomly receive at least one extra rare traits that is not available in the normal Tier 1 population such as; different designs in the background, mutations in the central Blob and changes in the Blobber's nature (transparency and glow). You can upgrade each Blobber once in "Your Blobbers" page. Your address should first be approved to perform any upgrades. Once it is approved, you can upgrade as many Tier 1 Blobbers as your BLB balance allows you to.
+                            </Paragraph>
                         </Card>
                     </Col>
                 </Row>
